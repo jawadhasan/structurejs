ab8a87b9c24f17c9845365d09603f4d055908c63
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-09-28 13:58:29 +0200

    dataservice basic

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
-import { Car} from "./model/car.js";
-import { Truck} from "./model/truck.js";
-
 import makeAjaxRequest from "./services/ajaxService.js";
+import {VehicleDataService} from "./services/vehicle-data-service.js";
+
+let vehicleDataService = new VehicleDataService();
 
 
  makeAjaxRequest('GET','./data/vehicles.json', null)
                     .done(function(data){
-                        console.log(data);
+                        vehicleDataService.loadData(data);
                     }).fail(function(e){
                         console.log(e);
-                    })
+                    });
 
 
 
-// let car1 = new Car('A123', 'Test Car1', 'Tesla');
-// console.log(`${car1.id} : ${car1.name}`);
-// car1.start();
-
-// let truck1 = new Truck('B444', 'Test Truck1',4);
-// console.log(`${truck1.id} : ${truck1.name}: ${truck1.storageArea}`);
-// truck1.start();
+//Testing
+function displayOutput(){
+    for(let car of vehicleDataService.cars)
+        console.log(car.name);
+}
+setTimeout(displayOutput,100);
--- src/services/vehicle-data-service.js
+++ src/services/vehicle-data-service.js
@@ -1,3 +1,6 @@
+import {Car} from '../model/car.js';
+import {Truck} from '../model/truck.js';
+
 export class VehicleDataService{
 
     constructor(){
@@ -5,7 +8,31 @@ export class VehicleDataService{
         this.trucks=[];
     }
 
-    loadData(data){
-        console.log(data);
+    loadData(vehicleData){
+        for(let data of vehicleData) {
+            switch(data.type){
+                
+                case 'car':
+                    let car = this.loadCar(data);
+                    this.cars.push(car);
+                    break;
+
+                case 'truck':
+                    let truck = this.loadTruck(data);
+                    this.trucks.push(truck);
+                    break;
+            }
+        }
     }
+
+    loadCar(data){
+        let car = new Car(data.id, data.name, data.make);
+        return car;
+    }
+
+    loadTruck(data){
+        let truck = new Truck(data.id, data.name, data.storageArea);
+        return truck;
+    }
+
 }
