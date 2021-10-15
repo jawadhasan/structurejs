b59a4ae7dc8c7acdff94b6b7cbcaf92c9841db6e
Jawad Shani <jawad.shani@hitachi-eu.com>
2021-10-15 15:27:18 +0200

    update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-free/css/all.css";
 
 import {Button} from './ui/button.js';
-import {Image} from './ui/image.js';
+
 
 let b = new Button('New Button!');
 b.appendToElement($('body')); //wrap it as jQuery object
